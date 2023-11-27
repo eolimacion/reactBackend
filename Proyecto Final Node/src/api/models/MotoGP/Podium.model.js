0b4f4c6428585b1fc7684a8eac0182772fdfa40f
abelgh91 <abelgh91@gmail.com>
2023-11-27 16:09:16 +0100

    camelcase

--- Proyecto Final Node/src/api/models/MotoGP/Podium.model.js
+++ Proyecto Final Node/src/api/models/MotoGP/Podium.model.js
@@ -5,9 +5,9 @@ const PodiumSchema = new Schema(
     {
         name: { type: String, unique: true, required: true },
         owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", unique: true },
-        firstplace: { type: mongoose.Schema.Types.ObjectId, ref: "Rider"},
-        secondplace: { type: mongoose.Schema.Types.ObjectId, ref: "Rider"},
-        thirdplace: { type: mongoose.Schema.Types.ObjectId, ref: "Rider"},
+        firstPlace: { type: mongoose.Schema.Types.ObjectId, ref: "Rider"},
+        secondPlace: { type: mongoose.Schema.Types.ObjectId, ref: "Rider"},
+        thirdPlace: { type: mongoose.Schema.Types.ObjectId, ref: "Rider"},
         circuit: { type: mongoose.Schema.Types.ObjectId, ref: "Circuit"},
         likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
         comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
