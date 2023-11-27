d7688dd858883c2a8cc1da08b779b6a6f22a08dc
marionovaro <marionovaro04@gmail.com>
2023-11-27 16:03:52 +0100

    modelo circuit

---
+++ Proyecto Final Node/src/api/models/MotoGP/Circuit.model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const CircuitSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    location: { type: String, required: true },
+    length: { type: Number, required: true },
+    capacity: { type: Number, required: true },
+    topSpeed: { type: Number, required: true },
+    image: { type: String, required: false },
+
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    selected: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    fastestLap: [{ type: mongoose.Schema.Types.ObjectId, ref: "Rider" }, { type: Number, required: true }, { type: Number, required: true }],
+    mostSuccessful: [{ type: mongoose.Schema.Types.ObjectId, ref: "Rider" }],
+  },
+  {
+    timestamps: true,
+  }
+);
+const Circuit = mongoose.model("Circuit", CircuitSchema);
+module.exports = Circuit
