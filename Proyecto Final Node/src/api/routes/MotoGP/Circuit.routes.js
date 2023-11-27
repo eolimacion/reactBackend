77fc0d03312f34fe4cf6bf40b354023fd7f3e606
Alsamu92 <alexsanzm@email.com>
2023-11-27 20:19:43 +0100

    circuitos y podium

--- Proyecto Final Node/src/api/routes/MotoGP/Circuit.routes.js
+++ Proyecto Final Node/src/api/routes/MotoGP/Circuit.routes.js
@@ -1,49 +1,21 @@
-const { upload } = require("../../middleware/files.middleware");
-const {
-  //! MAIN
-  create,
-  togglePlayer,
-  getById,
-  getAll,
-  getByName,
-  update,
-  deleteTeam,
-  sortTeamsbyLeagueandRanking,
 
-  //! EXTRA
-  sortTeamsbyDescending,
-  sortTeamsbyAscending,
-  filterGeneralNum,
-  filterAndSort,
-  averageStats,
+const { upload } = require("../../../middleware/files.middleware");
 
-  //! DESCARTADOS
-  sortTeamsbyPoints,
-  sortTeamsbyNetWorth,
-  add90players,
-} = require("../controllers/Team.controller");
+const { create, getById, getByName, getAll, update, deleteCircuit, sortCircuitsbyDescending, sortCircuitsbyAscending, filterGeneralNum, filterAndSort, averageStats} = require("../../controllers/MotoGP/Circuit.controller");
+
 
 const CircuitRoutes = require("express").Router();
 
-TeamRoutes.post("/", upload.single("image"), create);
-TeamRoutes.patch("/add/:id", togglePlayer);
-TeamRoutes.get("/:id", getById);
-TeamRoutes.get("/", getAll);
-TeamRoutes.get("/byName/:name", getByName);
-TeamRoutes.patch("/:id", upload.single("image"), update);
-TeamRoutes.delete("/:id", deleteTeam);
-
-//! Controladores Extra
-TeamRoutes.get("/sortdescending/teams/:stat", sortTeamsbyDescending);
-TeamRoutes.get("/sortascending/teams/:stat", sortTeamsbyAscending);
-TeamRoutes.get("/filter/teams/:filter/:gt/:lt", filterGeneralNum);
-TeamRoutes.get("/filtersort/teams/:filter/:gt/:lt", filterAndSort);
-TeamRoutes.get("/average/:stat/:teamId", averageStats);
-
-//! Controladores Descartados
-TeamRoutes.get("/sortbypoints/teams", sortTeamsbyPoints);
-TeamRoutes.get("/sortbynetworth/teams", sortTeamsbyNetWorth);
-TeamRoutes.patch("/players/:id/:players", add90players); //todo ---- REDIRECT
-TeamRoutes.get("/sortranking/:league", sortTeamsbyLeagueandRanking);
+CircuitRoutes.post("/", upload.single("image"), create);
+CircuitRoutes.get("/:id", getById);
+CircuitRoutes.get("/", getAll);
+CircuitRoutes.get("/byName/:name", getByName);
+CircuitRoutes.patch("/:id", upload.single("image"), update);
+CircuitRoutes.delete("/:id", deleteCircuit);
+CircuitRoutes.get("/sortdescending/circuits/:stat", sortCircuitsbyDescending);
+CircuitRoutes.get("/sortascending/circuits/:stat", sortCircuitsbyAscending);
+CircuitRoutes.get("/filter/circuits/:filter/:gt/:lt", filterGeneralNum);
+CircuitRoutes.get("/filtersort/circuits/:filter/:gt/:lt", filterAndSort);
+CircuitRoutes.get("/average/:stat/:circuitId", averageStats);
 
 module.exports = CircuitRoutes;
